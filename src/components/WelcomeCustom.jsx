c5669f36992f7eba2deb1cdfdb3d0d79bcb17941
fathia <adepojufathia04@gmail.com>
2026-01-27 00:56:32 -0800

    feat: enhance WelcomeCustom component with responsive text and animation adjustments

--- src/components/WelcomeCustom.jsx
+++ src/components/WelcomeCustom.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import styles from "./WelcomeStyles.module.css";
 
-function CustomSkeleton() {
+function WelcomeCustom() {
+  const [text, setText] = useState(
+    "EXPERIENCE PREMIUM SOUND LIKE NEVER BEFORE",
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setText("PREMIUM SOUND EXPERIENCE 😉😎");
+      } else {
+        setText("EXPERIENCE PREMIUM SOUND LIKE NEVER BEFORE🤗😉");
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={styles["custom-skeleton"]}>
-      <p className={styles["typing-animation-text"]}>
-        EXPERIENCE PREMIUM SOUND LIKE NEVER BEFORE
-      </p>
+      <p className={styles["typing-animation-text"]}>{text}</p>
     </div>
   );
 }
 
-export default CustomSkeleton;
+export default WelcomeCustom;
